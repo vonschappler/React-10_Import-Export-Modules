6ef61d14a88261de1987dd502a196daf54034ad0
von Schappler <vonschappler_dev@outlook.com>
2023-04-05 17:00:51 +0000

    Import with wildcards

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, {doublePi, triplePi} from './math'
+import * as pi from './math'
 
 ReactDOM.render(
   <ul>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
